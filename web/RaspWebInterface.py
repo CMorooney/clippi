32a5aacf48cbe76a8cc57c25d5293e358c4e6338
calvin <coolercal@gmail.com>
2023-12-09 13:21:58 +0000

    basic mp4 upload and listing of files on bank 01 on web interface

--- web/RaspWebInterface.py
+++ web/RaspWebInterface.py
@@ -1,31 +1,49 @@
 import os
+from os import listdir
+from os.path import isfile, join
+from pathlib import Path
 import RPi.GPIO as GPIO
-from flask import Flask, render_template, Response, request
+from flask import Flask, flash, render_template, Response, request
 import datetime
 
 app=Flask(__name__)
-    
+
+BANKS_PATH = '/home/calvin/App/__CONTENT'
+
+def bank_path(index):
+    paddedIndex = str(index).zfill(2)
+    return f'{BANKS_PATH}/{paddedIndex}';
+
+def allowed_file(filename):
+    return Path(filename).suffix == '.mp4'
+
 @app.route('/')
 def index():
-    now=datetime.datetime.now()
-    timeString=now.strftime("%Y-%m-%d %H:%M")
-    data=[]
-    templateData={
-        'title':'Raspberry Pi 3B+ Web Controller',
-        'time':timeString,
-        'data':data,
-    }
-    return render_template('index.html',**templateData)
+    bank_path_1 = bank_path(1)
+    onlyfiles = [f for f in listdir(bank_path_1) if isfile(join(bank_path_1, f))]
+    return render_template('index.html', files=onlyfiles)
 
 @app.route('/upload', methods = ['POST'])
 def upload():
     if request.method == 'POST':
-        f = request.files['file']
-        f.save(f'/home/calvin/App/__CONTENT/01/{f.filename}')
-        return render_template("index.html")
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return 'No file part'
+        else:
+            f = request.files['file']
+
+            # If the user does not select a file, the browser submits an
+            # empty file without a filename.
+            if f.filename == '':
+                return 'No selected file'
+            else:
+                if f and allowed_file(f.filename):
+                    f.save(f'{bank_path(1)}/{f.filename}')
+                else:
+                    return f'only .mp4 h264 files allowed {f.filename}'
+
+        return index()
 
 if __name__=='__main__':
     os.system("sudo rm -r  ~/.cache/chromium/Default/Cache/*")
     app.run(debug=True, port=5000, host='0.0.0.0',threaded=True)
-    #local web server http://192.168.1.200:5000/
-    #after Port forwarding Manipulation http://xx.xx.xx.xx:5000/
