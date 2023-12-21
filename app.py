13f9c740f78bef731a5675a4d540054c2fb9650f
calvin <coolercal@gmail.com>
2023-12-21 17:58:17 +0000

    remove debounce for shift key. encapsulate set_files and use less escape characters for playlist/file loading

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ load_dotenv(dotenv_path=dotenv_path)
 # get .env constants
 APP_PATH = os.getenv('APP_PATH')
 BANKS_PATH = os.getenv('BANKS_PATH')
-BANK_COUNT = os.getenv('BANK_COUNT')
+BANK_COUNT = int(os.getenv('BANK_COUNT'))
 
 # pixels in neopixel array
 total_pixels = 12
@@ -47,7 +47,7 @@ GPIO.setmode(GPIO.BCM)
 mplayer_command_queue = queue.Queue()
 
 # create bank directories if they don't exist
-for x in range(1, int(BANK_COUNT) + 1):
+for x in range(1, BANK_COUNT + 1):
     path = f'{BANKS_PATH}/{str(x).zfill(2)}'
     try:
         original_umask = os.umask(0)
@@ -79,9 +79,27 @@ power_led_pin = 27
 GPIO.setup(power_led_pin, GPIO.OUT, initial=GPIO.LOW)
 
 ##################################### mplayer start playlist
-files = sorted(os.listdir(BANKS_PATH + '/' + str(current_bank).zfill(2) + '/'))
+def set_files(bank):
+    global files
+    padded_bank_string = str(bank).zfill(2)
+    files = sorted(os.listdir(BANKS_PATH + '/' + padded_bank_string + '/'))
+
+def play_bank(bank):
+    set_files(bank)
+    padded_bank_string = str(bank).zfill(2)
+    for index, file in enumerate(files):
+      full_path = BANKS_PATH + '/' + padded_bank_string + '/' + file
+      base_command = 'loadfile ' + '"'+ full_path + '"'
+      if not index == 0:
+          base_command += " 1"
+      mplayer_command_queue.put(base_command + "\n")
+
+# we have to init mplayer with something to play, so create a playlist out of the current bank right away
+set_files(current_bank)
+
 # get videos in bank directory as a single string
-videos_string = ' '.join(sorted(map(lambda f: BANKS_PATH + '/' + str(current_bank).zfill(2) + '/' + f.replace(' ', '\ '), files)))
+videos_string = ' '.join(sorted(map(lambda f: BANKS_PATH + '/' + str(current_bank).zfill(2) + '/' + '"' + f + '"', files)))
+
 # start
 mplayer_spawn = spawn('/usr/bin/mplayer -fs -vo fbdev2 -nosound -vf scale=720:480 -loop 0 -slave -quiet ' + videos_string)
 
@@ -117,7 +135,6 @@ def hold_current_clip():
 
 def pending_bank_up():
     global pending_bank
-    # wrap-around
     if pending_bank == BANK_COUNT:
         pending_bank = 1
     else:
@@ -159,8 +176,8 @@ def shift_released():
     global current_bank
     global pending_bank
     if pending_bank != current_bank:
-        #todo somehow reload/reset mplayer playlist
         current_bank = pending_bank
+        play_bank(current_bank)
 
 def shift_button_changed(pin):
     global shift_pressed
@@ -173,7 +190,7 @@ GPIO.add_event_detect(prev_button_pin,       GPIO.FALLING, callback=button_pushe
 GPIO.add_event_detect(play_pause_button_pin, GPIO.FALLING, callback=button_pushed,        bouncetime=button_bounce_time)
 GPIO.add_event_detect(next_button_pin,       GPIO.FALLING, callback=button_pushed,        bouncetime=button_bounce_time)
 GPIO.add_event_detect(hold_button_pin,       GPIO.FALLING, callback=button_pushed,        bouncetime=button_bounce_time)
-GPIO.add_event_detect(shift_button_pin,      GPIO.BOTH,    callback=shift_button_changed, bouncetime=button_bounce_time)
+GPIO.add_event_detect(shift_button_pin,      GPIO.BOTH,    callback=shift_button_changed)
 
 ######################################### show power on
 GPIO.output(power_led_pin, GPIO.HIGH)
@@ -237,12 +254,15 @@ def mplayer_command_thread_execute():
       # request current file playing
       mplayer_spawn.write("pausing_keep_force get_property filename\n")
       mplayer_spawn.expect_exact(["ANS_filename="])
-      filename = str(mplayer_spawn.readline().rstrip()).replace('\'', '')
+      filename = str(mplayer_spawn.readline().rstrip()).replace('\'', '')[1:]
 
       # get the index of the filename in the global list of files
       # and use it to update the segmented display
-      clip_segment_update(files.index(filename[1:]))
+      if filename in files:
+          clip_segment_update(files.index(filename))
+
       bank_segment_update()
+
       # ensure colon is showing after updates, this can get overwritten for whatever reason
       segment_display.colon = True
 
